9bd5d70a807bdb7f691701ca399daefe4183ccb5
Keith Rodriguez <keithrodriguez@rightpoint.com>
2022-07-13 11:18:19 -0400

    Added image carousel

---
+++ src/Carousel.js
@@ -0,0 +1,50 @@
+import { Component } from "react";
+
+class Carousel extends Component {
+  // State of the Carousel: this.state
+  state = {
+    active: 0,
+  };
+
+  // Setting default props: this.props.images
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  handleIndexClick = (event) => {
+    // console.log(this);
+
+    this.setState({
+      // dataset is the data property defined in html(jsx) below.
+      // active: +event.target.dataset.index,
+      // We use "+" to make sure it is set as number, not string but could also use parseInt
+      active: parseInt(event.target.dataset.index),
+    });
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+              onClick={this.handleIndexClick}
+              data-index={index}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component {
   // No longer need constructor with newer JS, so using Parcel + babel plugin to transform our code.
@@ -29,11 +30,12 @@ class Details extends Component {
       return <h2> Loading...</h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
 
     return (
       <div className="details">
-        {/* <Carousel images={images} /> */}
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
